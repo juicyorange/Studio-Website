c18c857b1ea8dfccfac7a07717547458b7c1237d
juicyorange <rlaxodud9809@khu.ac.kr>
2026-01-06 23:05:05 +0900

    feat: 이미지 추가

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -1,35 +1,35 @@
-"use client";
-import Image from "next/image";
-import React from "react";
+'use client';
+import Image from 'next/image';
+import React from 'react';
 import {
   Modal,
   ModalBody,
   ModalContent,
   ModalFooter,
   ModalTrigger,
-} from "../ui/animated-modal";
-import { FloatingDock } from "../ui/floating-dock";
-import Link from "next/link";
+} from '../ui/animated-modal';
+import { FloatingDock } from '../ui/floating-dock';
+import Link from 'next/link';
 
-import SmoothScroll from "../smooth-scroll";
-import projects, { Project } from "@/data/projects";
-import { cn } from "@/lib/utils";
+import SmoothScroll from '../smooth-scroll';
+import projects, { Project } from '@/data/projects';
+import { cn } from '@/lib/utils';
 
 const ProjectsSection = () => {
   return (
-    <section id="projects" className="max-w-7xl mx-auto md:h-[130vh]">
-      <Link href={"#projects"}>
+    <section id='projects' className='max-w-7xl mx-auto md:h-[130vh]'>
+      <Link href={'#projects'}>
         <h2
           className={cn(
-            "bg-clip-text text-4xl text-center text-transparent md:text-7xl pt-16",
-            "bg-gradient-to-b from-black/80 to-black/50",
-            "dark:bg-gradient-to-b dark:from-white/80 dark:to-white/20 dark:bg-opacity-50 mb-32"
+            'bg-clip-text text-4xl text-center text-transparent md:text-7xl pt-16',
+            'bg-gradient-to-b from-black/80 to-black/50',
+            'dark:bg-gradient-to-b dark:from-white/80 dark:to-white/20 dark:bg-opacity-50 mb-32'
           )}
         >
           추억들
         </h2>
       </Link>
-      <div className="grid grid-cols-1 md:grid-cols-3">
+      <div className='grid grid-cols-1 md:grid-cols-3'>
         {projects.map((project, index) => (
           <Modall key={project.src} project={project} />
         ))}
@@ -39,29 +39,30 @@ const ProjectsSection = () => {
 };
 const Modall = ({ project }: { project: Project }) => {
   return (
-    <div className="flex items-center justify-center">
-          <div
-            className="relative w-[400px] h-auto rounded-lg overflow-hidden m-8"
-            style={{ aspectRatio: "3/2" }}
-          >
-            <Image
-              className="absolute w-full h-full top-0 left-0 hover:scale-[1.05] transition-all"
-              src={project.src}
-              alt={project.title}
-              width={300}
-              height={300}
-            />
-            <div className="absolute w-full h-1/2 bottom-0 left-0 bg-gradient-to-t from-black via-black/85 to-transparent pointer-events-none">
-              <div className="flex flex-col h-full items-start justify-end p-6">
-                <div className="text-lg text-left">{project.title}</div>
-                <div className="text-xs bg-white text-black rounded-lg w-fit px-2">
-                  {project.category}
-                </div>
-              </div>
+    <div className='flex items-center justify-center'>
+      <div
+        className='relative w-[400px] h-auto rounded-lg overflow-hidden m-8'
+        style={{ aspectRatio: '3/2' }}
+      >
+        <Image
+          style={{ objectFit: 'contain' }}
+          className='absolute w-full h-full top-0 left-0 hover:scale-[1.05] transition-all'
+          src={project.src}
+          alt={project.title}
+          unoptimized
+          width={300}
+          height={300}
+        />
+        <div className='absolute w-full h-1/2 bottom-0 left-0 bg-gradient-to-t from-black via-black/85 to-transparent pointer-events-none'>
+          <div className='flex flex-col h-full items-start justify-end p-6'>
+            <div className='text-lg text-left'>{project.title}</div>
+            <div className='text-xs bg-white text-black rounded-lg w-fit px-2'>
+              {project.category}
             </div>
           </div>
+        </div>
+      </div>
     </div>
   );
 };
 export default ProjectsSection;
-
--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,12 +1,12 @@
-import AceTernityLogo from "@/components/logos/aceternity";
-import SlideShow from "@/components/slide-show";
-import { Button } from "@/components/ui/button";
-import { TypographyH3, TypographyP } from "@/components/ui/typography";
-import { ArrowUpRight, ExternalLink, Link2, MoveUpRight } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { ReactNode } from "react";
-import { RiNextjsFill, RiNodejsFill, RiReactjsFill } from "react-icons/ri";
+import AceTernityLogo from '@/components/logos/aceternity';
+import SlideShow from '@/components/slide-show';
+import { Button } from '@/components/ui/button';
+import { TypographyH3, TypographyP } from '@/components/ui/typography';
+import { ArrowUpRight, ExternalLink, Link2, MoveUpRight } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { ReactNode } from 'react';
+import { RiNextjsFill, RiNodejsFill, RiReactjsFill } from 'react-icons/ri';
 import {
   SiChakraui,
   SiDocker,
@@ -26,34 +26,34 @@ import {
   SiThreedotjs,
   SiTypescript,
   SiVuedotjs,
-} from "react-icons/si";
-import { TbBrandFramerMotion } from "react-icons/tb";
-const BASE_PATH = "/assets/projects-screenshots";
+} from 'react-icons/si';
+import { TbBrandFramerMotion } from 'react-icons/tb';
+const BASE_PATH = '/assets/projects-screenshots';
 
 const ProjectsLinks = ({ live, repo }: { live: string; repo?: string }) => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-start gap-3 my-3 mb-8">
+    <div className='flex flex-col md:flex-row items-center justify-start gap-3 my-3 mb-8'>
       <Link
-        className="font-mono underline flex gap-2"
-        rel="noopener"
-        target="_new"
+        className='font-mono underline flex gap-2'
+        rel='noopener'
+        target='_new'
         href={live}
       >
-        <Button variant={"default"} size={"sm"}>
+        <Button variant={'default'} size={'sm'}>
           Visit Website
-          <ArrowUpRight className="ml-3 w-5 h-5" />
+          <ArrowUpRight className='ml-3 w-5 h-5' />
         </Button>
       </Link>
       {repo && (
         <Link
-          className="font-mono underline flex gap-2"
-          rel="noopener"
-          target="_new"
+          className='font-mono underline flex gap-2'
+          rel='noopener'
+          target='_new'
           href={repo}
         >
-          <Button variant={"default"} size={"sm"}>
+          <Button variant={'default'} size={'sm'}>
             Github
-            <ArrowUpRight className="ml-3 w-5 h-5" />
+            <ArrowUpRight className='ml-3 w-5 h-5' />
           </Button>
         </Link>
       )}
@@ -69,87 +69,87 @@ export type Skill = {
 };
 const PROJECT_SKILLS = {
   next: {
-    title: "Next.js",
-    bg: "black",
-    fg: "white",
+    title: 'Next.js',
+    bg: 'black',
+    fg: 'white',
     icon: <RiNextjsFill />,
   },
   chakra: {
-    title: "Chakra UI",
-    bg: "black",
-    fg: "white",
+    title: 'Chakra UI',
+    bg: 'black',
+    fg: 'white',
     icon: <SiChakraui />,
   },
   node: {
-    title: "Node.js",
-    bg: "black",
-    fg: "white",
+    title: 'Node.js',
+    bg: 'black',
+    fg: 'white',
     icon: <RiNodejsFill />,
   },
   python: {
-    title: "Python",
-    bg: "black",
-    fg: "white",
+    title: 'Python',
+    bg: 'black',
+    fg: 'white',
     icon: <SiPython />,
   },
   prisma: {
-    title: "prisma",
-    bg: "black",
-    fg: "white",
+    title: 'prisma',
+    bg: 'black',
+    fg: 'white',
     icon: <SiPrisma />,
   },
   postgres: {
-    title: "PostgreSQL",
-    bg: "black",
-    fg: "white",
+    title: 'PostgreSQL',
+    bg: 'black',
+    fg: 'white',
     icon: <SiPostgresql />,
   },
   mongo: {
-    title: "MongoDB",
-    bg: "black",
-    fg: "white",
+    title: 'MongoDB',
+    bg: 'black',
+    fg: 'white',
     icon: <SiMongodb />,
   },
   express: {
-    title: "Express",
-    bg: "black",
-    fg: "white",
+    title: 'Express',
+    bg: 'black',
+    fg: 'white',
     icon: <SiExpress />,
   },
   reactQuery: {
-    title: "React Query",
-    bg: "black",
-    fg: "white",
+    title: 'React Query',
+    bg: 'black',
+    fg: 'white',
     icon: <SiReactquery />,
   },
   shadcn: {
-    title: "ShanCN UI",
-    bg: "black",
-    fg: "white",
+    title: 'ShanCN UI',
+    bg: 'black',
+    fg: 'white',
     icon: <SiShadcnui />,
   },
   aceternity: {
-    title: "Aceternity",
-    bg: "black",
-    fg: "white",
+    title: 'Aceternity',
+    bg: 'black',
+    fg: 'white',
     icon: <AceTernityLogo />,
   },
   tailwind: {
-    title: "Tailwind",
-    bg: "black",
-    fg: "white",
+    title: 'Tailwind',
+    bg: 'black',
+    fg: 'white',
     icon: <SiTailwindcss />,
   },
   docker: {
-    title: "Docker",
-    bg: "black",
-    fg: "white",
+    title: 'Docker',
+    bg: 'black',
+    fg: 'white',
     icon: <SiDocker />,
   },
   yjs: {
-    title: "Y.js",
-    bg: "black",
-    fg: "white",
+    title: 'Y.js',
+    bg: 'black',
+    fg: 'white',
     icon: (
       <span>
         <strong>Y</strong>js
@@ -157,69 +157,69 @@ const PROJECT_SKILLS = {
     ),
   },
   firebase: {
-    title: "Firebase",
-    bg: "black",
-    fg: "white",
+    title: 'Firebase',
+    bg: 'black',
+    fg: 'white',
     icon: <SiFirebase />,
   },
   sockerio: {
-    title: "Socket.io",
-    bg: "black",
-    fg: "white",
+    title: 'Socket.io',
+    bg: 'black',
+    fg: 'white',
     icon: <SiSocketdotio />,
   },
   js: {
-    title: "JavaScript",
-    bg: "black",
-    fg: "white",
+    title: 'JavaScript',
+    bg: 'black',
+    fg: 'white',
     icon: <SiJavascript />,
   },
   ts: {
-    title: "TypeScript",
-    bg: "black",
-    fg: "white",
+    title: 'TypeScript',
+    bg: 'black',
+    fg: 'white',
     icon: <SiTypescript />,
   },
   vue: {
-    title: "Vue.js",
-    bg: "black",
-    fg: "white",
+    title: 'Vue.js',
+    bg: 'black',
+    fg: 'white',
     icon: <SiVuedotjs />,
   },
   react: {
-    title: "React.js",
-    bg: "black",
-    fg: "white",
+    title: 'React.js',
+    bg: 'black',
+    fg: 'white',
     icon: <RiReactjsFill />,
   },
   sanity: {
-    title: "Sanity",
-    bg: "black",
-    fg: "white",
+    title: 'Sanity',
+    bg: 'black',
+    fg: 'white',
     icon: <SiSanity />,
   },
   spline: {
-    title: "Spline",
-    bg: "black",
-    fg: "white",
+    title: 'Spline',
+    bg: 'black',
+    fg: 'white',
     icon: <SiThreedotjs />,
   },
   gsap: {
-    title: "GSAP",
-    bg: "black",
-    fg: "white",
-    icon: "",
+    title: 'GSAP',
+    bg: 'black',
+    fg: 'white',
+    icon: '',
   },
   framerMotion: {
-    title: "Framer Motion",
-    bg: "black",
-    fg: "white",
+    title: 'Framer Motion',
+    bg: 'black',
+    fg: 'white',
     icon: <TbBrandFramerMotion />,
   },
   supabase: {
-    title: "Supabase",
-    bg: "black",
-    fg: "white",
+    title: 'Supabase',
+    bg: 'black',
+    fg: 'white',
     icon: <SiSupabase />,
   },
 };
@@ -230,42 +230,83 @@ export type Project = {
   src: string;
 };
 const projects: Project[] = [
-    {
-      id: "1",
-      category: "24/6",
-      title: "첫 만남",
-      src: "/assets/memory/1.png",
-    },
-    {
-      id: "2",
-      category: "24/8",
-      title: "063XMerong party",
-      src: "/assets/memory/2.png",
-    },
-    {
-      id: "3",
-      category: "25/2",
-      title: "일본 여행",
-      src: "/assets/memory/3.png",
-    },
-    {
-      id: "4",
-      category: "25/2",
-      title: "영등포 어딘가",
-      src: "/assets/memory/4.png",
-    },
-    {
-      id: "5",
-      category: "25/6",
-      title: "천안 여행",
-      src: "/assets/memory/5.png",
-    },
-    {
-      id: "6",
-      category: "25/7",
-      title: "대부도",
-      src: "/assets/memory/6.png",
-    } 
-
+  {
+    id: '1',
+    category: '25/1',
+    title: '사귄 첫번째 달',
+    src: '/assets/memory/1.png',
+  },
+  {
+    id: '2',
+    category: '25/2',
+    title: '처음으로 같이간 여행',
+    src: '/assets/memory/2.png',
+  },
+  {
+    id: '3',
+    category: '25/3',
+    title: '함께한 지희 생일',
+    src: '/assets/memory/3.png',
+  },
+  {
+    id: '4',
+    category: '25/3',
+    title: '첫 제주도 여행',
+    src: '/assets/memory/4.png',
+  },
+  {
+    id: '5',
+    category: '25/4',
+    title: '벚꽃 핀 4월',
+    src: '/assets/memory/5.png',
+  },
+  {
+    id: '6',
+    category: '25/5',
+    title: '무도 마라톤',
+    src: '/assets/memory/6.png',
+  },
+  {
+    id: '7',
+    category: '24/6',
+    title: '워터파크&연희동 전시회',
+    src: '/assets/memory/7.png',
+  },
+  {
+    id: '8',
+    category: '24/7',
+    title: '부산 여행',
+    src: '/assets/memory/8.png',
+  },
+  {
+    id: '9',
+    category: '25/8',
+    title: '함께 다시 간 제주도',
+    src: '/assets/memory/9.png',
+  },
+  {
+    id: '10',
+    category: '25/9',
+    title: '경복궁 야간 개장',
+    src: '/assets/memory/10.png',
+  },
+  {
+    id: '11',
+    category: '25/10',
+    title: '이것저것',
+    src: '/assets/memory/11.png',
+  },
+  {
+    id: '12',
+    category: '25/11',
+    title: '이모저모',
+    src: '/assets/memory/12.png',
+  },
+  {
+    id: '13',
+    category: '25/12',
+    title: '크리스마스&25년 마지막 날',
+    src: '/assets/memory/13.png',
+  },
 ];
 export default projects;
